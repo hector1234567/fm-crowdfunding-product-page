f5af48421cb9214f41f8a34f2ec1df962b9faa27
Hector B <hectorbg303@gmail.com>
2025-08-20 23:31:19 +0200

    modal products

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.querySelector(".burguer");
 const primaryNavigation = document.querySelector(".primary-navigation");
+const rewardBtns = document.querySelectorAll(".card a.button");
 
 burgerBtn.addEventListener("click", function (ev) {
   const expanded = burgerBtn.getAttribute("aria-expanded");
@@ -8,4 +9,15 @@ burgerBtn.addEventListener("click", function (ev) {
   } else {
     burgerBtn.setAttribute("aria-expanded", "false");
   }
+});
+
+rewardBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const priceId = btn.dataset.price;
+    document.getElementById(priceId).checked = true;
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
